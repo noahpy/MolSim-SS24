25f801b412e18ec13f0738b2fcf81ca5b4814470
noahpy <noah.schlenker2002@gmail.com>
2024-04-28 22:43:11 +0200

    first small test

--- tests/container/testParticleContainer.cpp
+++ tests/container/testParticleContainer.cpp
@@ -1,16 +1,21 @@
 
-#include "ParticleContainer.h"
 #include "Particle.h"
+#include "ParticleContainer.h"
 #include <gtest/gtest.h>
+#include <iostream>
 #include <vector>
 
 TEST(PContainerTests, enhancedForLoopParticles)
 {
     // TODO implement test
-    
-    std::vector<Particle> particles{};
 
-    ParticleContainer p {};
+    std::vector<Particle> particles { Particle {}, Particle {} };
+
+    ParticleContainer container { particles };
+
+    for (auto& p : container) {
+        std::cout << p.getM() << std::endl;
+    }
 
     EXPECT_EQ(true, true);
 }
