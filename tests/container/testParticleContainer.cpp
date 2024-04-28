250665b0e49aded6939a57bd1464430924b57bf3
noahpy <noah.schlenker2002@gmail.com>
2024-04-29 01:45:57 +0200

    add pair loop test

--- tests/container/testParticleContainer.cpp
+++ tests/container/testParticleContainer.cpp
@@ -2,20 +2,44 @@
 #include "Particle.h"
 #include "ParticleContainer.h"
 #include <gtest/gtest.h>
-#include <iostream>
+#include <ostream>
 #include <vector>
 
+// check if enhanced loop work as intended
 TEST(PContainerTests, enhancedForLoopParticles)
 {
-    // TODO implement test
+    std::array<double, 3> zeros{0, 0, 0};
 
-    std::vector<Particle> particles { Particle {}, Particle {} };
+    Particle p1{zeros, zeros, 5, 0};
+    Particle p2{zeros, zeros, 3, 0};
+    Particle p3{zeros, zeros, 1, 0};
+
+    std::vector<Particle> particles { p1, p2, p3 };
 
     ParticleContainer container { particles };
 
+    double result = 0;
     for (auto& p : container) {
-        std::cout << p.getM() << std::endl;
+        result += p.getM();
     }
 
-    EXPECT_EQ(true, true);
+    EXPECT_EQ(result, 9);
+}
+
+TEST(PContainerTests, pairIterators)
+{
+    std::array<double, 3> zeros{0, 0, 0};
+
+    Particle p1{zeros, zeros, 5, 0};
+    Particle p2{zeros, zeros, 3, 0};
+    Particle p3{zeros, zeros, 1, 0};
+
+    std::vector<Particle> particles { p1, p2, p3 };
+
+    ParticleContainer container { particles };
+
+    for(auto it = container.beginPairs(); it != container.endPairs(); ++it){
+        std::pair<Particle&, Particle&> pt_pair = *it;
+        std::cout << pt_pair.first.getM() << " | " << pt_pair.second.getM() << std::endl;
+    }
 }
