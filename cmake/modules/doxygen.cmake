9c3f00c1aa7317ca75cd9bdd8eb3e699d4327ded
Christian Nix <Christian-Nix@gmx.net>
2024-04-30 00:07:26 +0200

    Added the doc_doxygen target to build documentation. Needs testing still

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,25 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+# Check if Doxygen is installed
+find_package(Doxygen)
 
-# TODO: create CMake build option for the target.
+# Dont do anything if Doxygen is not installed
+if (DOXYGEN_FOUND)
+    # Create an option to turn on/off the generation of the documentation
+    option(GENERATE_DOC "Generate documentation with Doxygen" ON)
+    if (GENERATE_DOC)
+        # set input Doxyfile and output directory for the documentation
+        set(DOXYGEN_CONFIG ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile)
+        set(DOCUMENTATION_OUT ${CMAKE_CURRENT_BINARY_DIR}/documentation)
 
-# TODO: Add a custom target for building the documentation.
+        # request to configure the file
+        # @ONLY will only replace variables of the form @VAR@
+        configure_file(${DOXYGEN_CONFIG} ${DOCUMENTATION_OUT} @ONLY)
+
+        # add a target to generate the documentation
+        # i.e. create documentation by make doc_doxygen
+        add_custom_target(doc_doxygen
+                COMMAND ${DOXYGEN_EXECUTABLE} ${DOCUMENTATION_OUT}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMENT "Generating documentation with Doxygen"
+                VERBATIM)
+    endif (GENERATE_DOC)
+endif (DOXYGEN_FOUND)
