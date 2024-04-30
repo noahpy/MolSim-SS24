b8492910c18bb857976a2948a6f5663a57dc9d63
Christian Nix <Christian-Nix@gmx.net>
2024-04-30 15:47:20 +0200

    Fix doxygen cmake. Wrong working dir was set i.e. no files were documented. Also simplyfied the code to remove uncessary parts from tutorial

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -6,19 +6,14 @@ if (DOXYGEN_FOUND)
     # Create an option to turn on/off the generation of the documentation
     option(GENERATE_DOC "Generate documentation with Doxygen" ON)
     if (GENERATE_DOC)
-        # set input Doxyfile and output directory for the documentation
+        # set input Doxyfile
         set(DOXYGEN_CONFIG ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile)
-        set(DOCUMENTATION_OUT ${CMAKE_CURRENT_BINARY_DIR}/documentation)
-
-        # request to configure the file
-        # @ONLY will only replace variables of the form @VAR@
-        configure_file(${DOXYGEN_CONFIG} ${DOCUMENTATION_OUT} @ONLY)
 
         # add a target to generate the documentation
         # i.e. create documentation by make doc_doxygen
         add_custom_target(doc_doxygen
-                COMMAND ${DOXYGEN_EXECUTABLE} ${DOCUMENTATION_OUT}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_CONFIG}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} # Folder to use as root to discover the input files
                 COMMENT "Generating documentation with Doxygen"
                 VERBATIM)
     endif (GENERATE_DOC)
