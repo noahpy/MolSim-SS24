79bef76e5bb05a90abbcad6427c5d2ba673d3d4f
noahpy <noah.schlenker2002@gmail.com>
2024-05-01 09:10:31 +0200

    downcast in physics functions

--- src/physics/velocityCal/velocityCal.cpp
+++ src/physics/velocityCal/velocityCal.cpp
@@ -1,11 +1,14 @@
 
 #include "physics/velocityCal/velocityCal.h"
+#include "simulation/planetSim.h"
 #include "utils/ArrayUtils.h"
 
 void velocity_stroemer_verlet(const Simulation& sim)
 {
-    for (auto& p : sim.container) {
+    // downcast if necessary
+    const PlanetSimulation& sim_downcast = dynamic_cast<const PlanetSimulation&>(sim);
+    for (auto& p : sim_downcast.container) {
         // v = v + Δt * (F + F_old) / (2 * m)
-        p.setV(p.getV() + (sim.delta_t / (2 * p.getM())) * (p.getOldF() + p.getF()));
+        p.setV(p.getV() + (sim_downcast.delta_t / (2 * p.getM())) * (p.getOldF() + p.getF()));
     }
 }
