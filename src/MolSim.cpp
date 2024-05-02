b40ac2bec82fab9824becb2ee31eeee318e40e83
noahpy <noah.schlenker2002@gmail.com>
2024-05-02 15:01:05 +0200

    correct wrong force calculation name

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argsv[])
     // Intialize physics strategy
     PhysicsStrategy strat { location_stroemer_verlet,
                             velocity_stroemer_verlet,
-                            force_stroemer_verlet_rv };
+                            force_stroemer_verlet_V2 };
 
     // Intialize empty particle container
     ParticleContainer particles { {} };
