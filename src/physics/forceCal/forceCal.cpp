bb7ad91fec5426fd454717a9bce5aa49a053597d
noahpy <noah.schlenker2002@gmail.com>
2024-05-02 14:09:30 +0200

    tidy up force calculation

--- src/physics/forceCal/forceCal.cpp
+++ src/physics/forceCal/forceCal.cpp
@@ -23,7 +23,7 @@ void force_stroemer_verlet(const Simulation& sim)
     }
 }
 
-void force_stroemer_verlet_rv(const Simulation& sim)
+void force_stroemer_verlet_V2(const Simulation& sim)
 {
     std::array<double, 3> zeros { 0, 0, 0 };
     for (auto& p : sim.container) {
@@ -46,41 +46,3 @@ void force_stroemer_verlet_rv(const Simulation& sim)
     }
 }
 
-void force_stroemer_verlet_V2(const Simulation& sim)
-{
-    // map for indices of the particle container
-    std::unordered_map<const Particle*, size_t> particleIndexMap;
-    const auto& particles = sim.container.getContainer();
-    for (size_t i = 0; i < particles.size(); ++i) {
-        particleIndexMap[&particles[i]] = i;
-    }
-
-    // Force vector for storing computed F_i
-    std::vector<std::array<double, 3>> f_i(sim.container.getContainer().size(), { 0.0, 0.0, 0.0 });
-
-    // Force Calculation with unique pairs by adding F_ij and F_ji = - F_ij to the right forces
-    for (auto it = sim.container.beginPairs(); it != sim.container.endPairs(); ++it) {
-        auto pair = *it;
-        Particle& p1 = pair.first;
-        Particle& p2 = pair.second;
-
-        size_t index1 = particleIndexMap[&p1];
-        size_t index2 = particleIndexMap[&p2];
-
-        double m_mul = p1.getM() * p2.getM();
-        double dist = std::pow(ArrayUtils::L2Norm(p1.getX() - p2.getX()), 3);
-        double coeff = m_mul / dist;
-        std::array<double, 3> f_ij = coeff * (p2.getX() - p1.getX());
-
-        f_i[index1] = f_i[index1] + f_ij;
-        f_i[index2] = f_i[index2] - f_ij;
-    }
-
-    // Storing computed Forces to particles
-    int k = 0;
-    for (auto& p : sim.container) {
-        p.setOldF(p.getF());
-        p.setF(f_i[k]);
-        ++k;
-    }
-}
