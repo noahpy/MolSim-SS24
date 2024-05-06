a1a1a190015bed6b38657944367c112ee14e9437
Christian Nix <Christian-Nix@gmx.net>
2024-05-06 18:19:26 +0200

    Initial implementation of forces using lennard jones

--- src/physics/forceCal/forceCal.cpp
+++ src/physics/forceCal/forceCal.cpp
@@ -46,3 +46,33 @@ void force_stroemer_verlet_V2(const Simulation& sim)
     }
 }
 
+void force_lennard_jones(const Simulation& sim)
+{
+    std::array<double, 3> zeros { 0, 0, 0 };
+    for (auto& p : sim.container) {
+        p.setOldF(p.getF());
+        p.setF(zeros);
+    }
+
+    for (auto it = sim.container.beginPairs(); it != sim.container.endPairs(); ++it) {
+        auto pair = *it;
+        Particle& p1 = pair.first;
+        Particle& p2 = pair.second;
+
+        // This is constant => maybe we should move this into a class to set it up in the constructor
+        double epsilon = 1.0; // set in simulation i.e. access here by sim.epsilon
+        double sigma = 1.0; // set in simulation i.e. access here by sim.sigma
+        double alpha = -24 * epsilon;
+        double beta = std::pow(sigma, 6);
+        double gamma = -2 * std::pow(beta, 2);
+
+        std::array<double, 3> delta = p1.getX()-p2.getX();
+        double dotDelta = ArrayUtils::DotProduct(p1.getX());
+        double dotDelta3 = std::pow(dotDelta, 3);
+        double dotDelta6 = std::pow(dotDelta3, 2);
+
+        std::array<double, 3> force = (alpha/dotDelta) * (beta / dotDelta3 + gamma / dotDelta6) * delta;
+        p1.setF(p1.getF() + force);
+        p2.setF(p2.getF() - force);
+    }
+}
