166fdb3fb1d9167255fbe9d9fe685a671fb6f1c5
noahpy <noschl@proton.me>
2024-05-07 14:14:52 +0200

    format code

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -31,9 +31,7 @@ int main(int argc, char* argsv[])
     outputWriter::VTKWriter writer;
 
     // Intialize physics strategy
-    PhysicsStrategy strat { location_stroemer_verlet,
-                            velocity_stroemer_verlet,
-                            force_gravity_V2 };
+    PhysicsStrategy strat { location_stroemer_verlet, velocity_stroemer_verlet, force_gravity_V2 };
 
     // Intialize empty particle container
     ParticleContainer particles { {} };
