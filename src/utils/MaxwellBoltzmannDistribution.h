339e702127ef4cbfe1c6c15206da1315ce760cb1
noahpy <noschl@proton.me>
2024-05-15 16:13:36 +0200

    add inline keyword

--- src/utils/MaxwellBoltzmannDistribution.h
+++ src/utils/MaxwellBoltzmannDistribution.h
@@ -19,7 +19,7 @@
  * to 2 or 3.
  * @return Array containing the generated velocity vector.
  */
-std::array<double, 3> maxwellBoltzmannDistributedVelocity(double averageVelocity, size_t dimensions)
+inline std::array<double, 3> maxwellBoltzmannDistributedVelocity(double averageVelocity, size_t dimensions)
 {
     // we use a constant seed for repeatability.
     // random engine needs static lifetime otherwise it would be recreated for every call.
