55243b0d613c43da2a352bc30f8ec048c8a2124a
Christian Nix <Christian-Nix@gmx.net>
2024-05-21 14:48:59 +0200

    Move param checking to parent

--- src/models/generators/CuboidParticleCluster.cpp
+++ src/models/generators/CuboidParticleCluster.cpp
@@ -27,14 +27,6 @@ size_t CuboidParticleCluster::getTotalNumberOfParticles() const
 void CuboidParticleCluster::generateCluster(
     std::vector<Particle>& particles, size_t& insertionIndex) const
 {
-    if (dimensions != 2 && dimensions != 3) {
-        spdlog::warn(
-            "The dimensions specified for a Cuboid Particle cluster was set to {}. It can only be "
-            "of values 2 or 3",
-            dimensions);
-        throw std::invalid_argument("Dimensions must be 2 or 3.");
-    }
-
     for (int i = 0; i < numParticlesWidth; i++) { // x
         for (int j = 0; j < numParticlesHeight; j++) { // y
             for (int k = 0; k < numParticlesDepth; k++) { // z
--- src/models/generators/ParticleCluster.cpp
+++ src/models/generators/ParticleCluster.cpp
@@ -13,4 +13,11 @@ ParticleCluster::ParticleCluster(
     , meanVelocity(meanVelocity)
     , dimensions(dimensions)
 {
+    if (dimensions != 2 && dimensions != 3) {
+        spdlog::warn(
+            "The dimensions specified for the brownian motion of a particle cluster was set to {}. "
+            "It can only be of values 2 or 3",
+            dimensions);
+        throw std::invalid_argument("Dimensions must be 2 or 3.");
+    }
 }
