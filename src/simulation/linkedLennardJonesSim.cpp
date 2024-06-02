8de3cb26b79916b9b9c9066f50b4b489ac78c8ce
noahpy <noschl@proton.me>
2024-06-02 03:03:19 +0200

    add linked lj simulation class

---
+++ src/simulation/linkedLennardJonesSim.cpp
@@ -0,0 +1,29 @@
+
+#include "simulation/linkedLennardJonesSim.h"
+
+LinkedLennardJonesSimulation::LinkedLennardJonesSimulation(
+    double time,
+    double delta_t,
+    double end_time,
+    ParticleContainer& container,
+    PhysicsStrategy& strat,
+    std::unique_ptr<FileWriter> writer,
+    std::unique_ptr<FileReader> reader,
+    double epsilon,
+    double sigma,
+    std::array<double, 3> domainOrigin,
+    std::array<double, 3> domainSize,
+    double cutoff)
+    : LennardJonesSimulation(
+          time,
+          delta_t,
+          end_time,
+          container,
+          strat,
+          std::move(writer),
+          std::move(reader),
+          epsilon,
+          sigma)
+    , cellGrid(domainOrigin, domainSize, cutoff)
+{
+}
