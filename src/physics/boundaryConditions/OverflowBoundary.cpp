1d4736742e29d17b9bc7576b29d64287f1a3e402
noahpy <noschl@proton.me>
2024-06-17 21:54:05 +0200

    remove some logs

--- src/physics/boundaryConditions/OverflowBoundary.cpp
+++ src/physics/boundaryConditions/OverflowBoundary.cpp
@@ -2,8 +2,6 @@
 #include "OverflowBoundary.h"
 #include "models/linked_cell/cell/Cell.h"
 #include "simulation/LennardJonesDomainSimulation.h"
-#include <iostream>
-#include <unordered_map>
 
 void OverflowBoundary::preUpdateBoundaryHandling(Simulation& simulation)
 {
@@ -15,21 +13,13 @@ void OverflowBoundary::preUpdateBoundaryHandling(Simulation& simulation)
     for (auto haloCellIndex : LGDSim.getGrid().haloCellIterator(position)) {
         // splice moves the pointers within the list, clearing out the particle list of the halo
         // cell
-        spdlog::info("Size {}", LGDSim.getGrid()
-                         .cells[haloCellIndex[0]][haloCellIndex[1]][haloCellIndex[2]]
-                         ->getParticles().size());
         particlesToRemove.splice(
             particlesToRemove.end(),
             LGDSim.getGrid()
                 .cells[haloCellIndex[0]][haloCellIndex[1]][haloCellIndex[2]]
                 ->getParticles());
-        spdlog::info("Size {}", LGDSim.getGrid()
-                                    .cells[haloCellIndex[0]][haloCellIndex[1]][haloCellIndex[2]]
-                                    ->getParticles().size());
     }
 
-    // create a map to check in O(1) if a particle is in the list
-    std::unordered_map<Particle*, bool> particlesToRemoveMap {};
     for (const auto& particle : particlesToRemove) {
         spdlog::info("Deleted {}", particle.get().toString());
         simulation.container.removeParticle(particle);
