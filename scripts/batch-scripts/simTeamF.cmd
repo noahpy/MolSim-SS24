2307cc35c67eec82eb00d1aa8643863b12fb5e46
noahpy <noschl@proton.me>
2024-06-19 04:50:40 +0200

    add some details

--- scripts/batch-scripts/simTeamF.cmd
+++ scripts/batch-scripts/simTeamF.cmd
@@ -4,7 +4,6 @@
 #SBATCH -D ./MolSim-SS24/build
 #SBATCH --get-user-env
 #SBATCH --clusters=serial
-#SBATCH --partition=serial_std
 #SBATCH --mem=1000mb
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
