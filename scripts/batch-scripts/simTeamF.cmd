40d99e331e083367a74232713135716bcec35f85
noahpy <noschl@proton.me>
2024-06-19 15:22:50 +0200

    update batch script

--- scripts/batch-scripts/simTeamF.cmd
+++ scripts/batch-scripts/simTeamF.cmd
@@ -2,10 +2,12 @@
 #SBATCH -J simTeamF
 #SBATCH -D ./MolSim-SS24/build
 #SBATCH --get-user-env
+#SBATCH --clusters=serial
+#SBATCH --partition=serial_std
 #SBATCH --mem=1000mb
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
-#SBATCH --mail-user=noah.schlenker@tum.de
+#SBATCH --mail-user=ge42joq@mytum.de
 #SBATCH --time=00:30:00
 
 src/MolSim ../input/eingabe-sonne.txt -e 1000 
