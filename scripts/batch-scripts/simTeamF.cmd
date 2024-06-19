a424d8524488de4777e361efa183188abbc753e7
noahpy <noschl@proton.me>
2024-06-19 04:44:42 +0200

    add email

--- scripts/batch-scripts/simTeamF.cmd
+++ scripts/batch-scripts/simTeamF.cmd
@@ -8,6 +8,7 @@
 #SBATCH --mem=1000mb
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
+#SBATCH --mail-user=noah.schlenker@tum.de
 #SBATCH --time=00:30:00
 
 src/MolSim ../input/eingabe-sonne.txt -e 1000 
