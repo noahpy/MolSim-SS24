2d15849f57756a921b05f6171e1ed5d6f199ae23
Christian Nix <Christian-Nix@gmx.net>
2024-07-12 18:01:55 +0200

    Update builder

--- scripts/batch-scripts/buildUniversalNanoFlow.sh
+++ scripts/batch-scripts/buildUniversalNanoFlow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf MolSim-SS24-NanoFlow
-git clone https://github.com/noahpy/MolSim-SS24.git -b nanoFlowRunner
+git clone https://github.com/noahpy/MolSim-SS24.git MolSim-SS24-NanoFlow -b nanoFlowRunner
 cd MolSim-SS24-NanoFlow
 ./scripts/batch-scripts/loadModule.sh
 mkdir build && cd build
