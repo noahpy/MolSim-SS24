518d6e8c929b3a4abdac2d672e5dcafc94865ac0
noahpy <noschl@proton.me>
2024-07-16 23:25:27 +0200

    allow change of parllelization strat from command line

--- src/physics/stratFactory.h
+++ src/physics/stratFactory.h
@@ -8,8 +8,10 @@
  *
  * This function creates and returns a PhysicsStrategy object based on the simulation type provided.
  *
- * @param simulation_type An unsigned integer representing the type of simulation strategy to
+ * @param simulation_type An enum representing the type of simulation strategy to
  * create.
+ * @param parallel_type An enum representing the type of parallelization to use.
  * @return A PhysicsStrategy object representing the appropriate simulation strategy.
  */
-PhysicsStrategy stratFactory(SimulationType simulation_type);
+PhysicsStrategy stratFactory(
+    SimulationType simulation_type, ParallelType parallel_type = ParallelType::STATIC);
