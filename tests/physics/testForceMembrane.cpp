754f0d0142fbfd32aae6fed6cc86fde0c4468e5a
Christian Nix <Christian-Nix@gmx.net>
2024-07-16 18:48:59 +0200

    Added last test

--- tests/physics/testForceMembrane.cpp
+++ tests/physics/testForceMembrane.cpp
@@ -2,6 +2,7 @@
 #include "io/fileReader/emptyReader.h"
 #include "io/fileWriter/VTKWriter.h"
 #include "models/Particle.h"
+#include "models/molecules/Membrane.h"
 #include "physics/boundaryConditions/BoundaryConfig.h"
 #include "physics/forceCal/forceCal.h"
 #include "physics/locationCal/locationCal.h"
@@ -9,15 +10,17 @@
 #include "physics/velocityCal/velocityCal.h"
 #include "simulation/MembraneSimulation.h"
 #include "simulation/baseSimulation.h"
+#include "utils/ArrayUtils.h"
 #include <cmath>
 #include <gtest/gtest.h>
+
 
 class calcForceMembrane : public ::testing::Test {
 protected:
     double start_time;
     double end_time;
     double delta_t;
-    double PRESICION = 10e-5;
+    double PRESICION = 1e-8;
     std::unique_ptr<EmptyFileReader> fileReader;
     std::unique_ptr<outputWriter::VTKWriter> writer;
     PhysicsStrategy strat;
@@ -25,12 +28,13 @@ protected:
     std::array<double, 3> domainOrigin = { -5, -5, -5 };
     std::array<double, 3> domainSize = { 10, 10, 10 };
     const double cutoff = 2.5;
-    std::array<double, 3> membraneOrigin = { 0, 0, 0};
+    std::array<double, 3> membraneOrigin = { 0, 0, 0 };
     int numParticlesWidth = 3;
     int numParticlesHeight = 1;
     double k = 1;
     double r_0 = 1;
     double spacing = 2;
+    std::array<double, 3> z = { 0, 0, 0 };
 
     calcForceMembrane()
         : start_time(0)
@@ -44,202 +48,135 @@ protected:
     }
 };
 
-// Test force calculations for linear molecules
+// Test intra molecular force calculations for linear molecules against hand calculated values
 TEST_F(calcForceMembrane, calcForceMembrane)
 {
-    Particle p1 { { 0, 0, 0 }, { 0, 0, 0 }, 1, 3 };
-    Particle p2 { { 2, 0, 0 }, { 0, 0, 0 }, 1, 3 };
-    Particle p3 { { 4, 0, 0 }, { 0, 0, 0 }, 1, 3 };
+    double r0 = 2;
+    double r = 1;
+    double k = 20;
 
-    particles = std::vector<Particle> { p1, p2, p3 };
+    particles = std::vector<Particle> {};
+    ParticleContainer container { particles };
+    Particle tmp;
+    Membrane mem { z, 2, 1, 1, r, 1, z, 0, 3, 1, r0, k, tmp };
+    mem.generateMolecule(container, 562);
 
-    double epsilon = 3.14159;
-    double sigma = 1;
-    double gravityConst = 0;
+    // Check if the number of particles are correct
+    EXPECT_EQ(container.particles.size(), 2);
 
-    std::array<double, 3> p1F { 1, 0, 0 };
-    std::array<double, 3> p2F { 0, 0, 0 };
-    std::array<double, 3> p3F { -1, 0, 0 };
-    std::array<std::array<double, 3>, 3> expectedFs = { p1F, p2F, p3F };
+    CellGrid grid { { -5, -5, -5 }, { 10, 10, 10 }, 5 };
+    grid.addParticlesFromContainer(container);
 
-    std::map<unsigned, std::pair<double, double>> LJParams {
-        { 1, { epsilon, sigma }}, { 2, { epsilon, sigma }}, { 3, { epsilon, sigma } }
-    };
+    mem.calculateIntraMolecularForces(grid);
 
-    MembraneSimulation sim(
-        start_time,
-        delta_t,
-        end_time,
-        particles,
-        strat,
-        std::move(writer),
-        std::move(fileReader),
-        LJParams,
-        domainOrigin,
-        domainSize,
-        cutoff,
-        membraneOrigin,
-        numParticlesWidth,
-        numParticlesHeight,
-        k,
-        r_0,
-        spacing,
-        BoundaryConfig(
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW),
-        gravityConst,
-        0,
-        0,
-        0);
+    // This is simply the harmonic potential
+    std::array<double, 3> expectedForce =
+        k / r * (r - r0) * (container.particles[1].getX() - container.particles[0].getX());
 
-    force_membrane(sim);
-
-    unsigned pCount = 0;
-    for (auto p : particles) {
-        for (unsigned i = 0; i < 3; i++) {
-            EXPECT_NEAR(p.getF().at(i), expectedFs.at(pCount).at(i), PRESICION)
-                << "Particle " << pCount << " Dimension " << i << ": Expected "
-                << expectedFs.at(pCount).at(i) << " but got: " << p.getF().at(i);
-        }
-        ++pCount;
+    for (int i = 0; i < 3; i++) {
+        // Newtons third law
+        EXPECT_NEAR(container.particles[0].getF().at(i), expectedForce.at(i), PRESICION);
+        EXPECT_NEAR(container.particles[1].getF().at(i), -expectedForce.at(i), PRESICION);
     }
 }
 
-// Test forces for diagonal molecules particles
+// Test intra molecular force calculations for diagonal molecules against hand calculated values
 TEST_F(calcForceMembrane, calcForceMembraneDiagonal)
 {
-    // Align particles in a diagonal manner inside the molecules
-    Particle p1 { { 0, 0, 0 }, { 0, 0, 0 }, 1, 3 };
-    Particle p2 { { 2, 2, 0 }, { 0, 0, 0 }, 1, 3 };
-    Particle p3 { { 4, 0, 0 }, { 0, 0, 0 }, 1, 3 };
+    double r0 = 2;
+    double r = 1;
+    double k = 20;
 
-    particles = std::vector<Particle> { p1, p2, p3 };
+    particles = std::vector<Particle> {};
+    ParticleContainer container { particles };
+    Particle tmp;
+    Membrane mem { z, 2, 2, 1, r, 1, z, 0, 3, 1, r0, k, tmp };
+    mem.initLJParams(1, 0.1); // small sigma to ignore repulsive forces
+    mem.generateMolecule(container, 562);
 
-    double epsilon = 3.14159;
-    double sigma = 1;
-    double gravityConst = 0;
+    // Check if the number of particles are correct
+    EXPECT_EQ(container.particles.size(), 4);
 
-    std::array<double, 3> p1F { 1, 1, 0 };
-    std::array<double, 3> p2F { 0, -2, 0 };
-    std::array<double, 3> p3F { -1, 1, 0 };
-    std::array<std::array<double, 3>, 3> expectedFs = { p1F, p2F, p3F };
+    CellGrid grid { { -5, -5, -5 }, { 10, 10, 10 }, 5 };
+    grid.addParticlesFromContainer(container);
 
-    std::map<unsigned, std::pair<double, double>> LJParams {
-            { 1, { epsilon, sigma }}, { 2, { epsilon, sigma }}, { 3, { epsilon, sigma } }
+    mem.calculateIntraMolecularForces(grid);
+
+    // lambda function to do harmonic potential
+    auto harmonicPotential = [](Particle& p1, Particle& p2, double r0, double k) {
+        return k / ArrayUtils::L2Norm(p2.getX() - p1.getX()) *
+               (ArrayUtils::L2Norm(p2.getX() - p1.getX()) - r0) * (p2.getX() - p1.getX());
     };
 
-    MembraneSimulation sim(
-        start_time,
-        delta_t,
-        end_time,
-        particles,
-        strat,
-        std::move(writer),
-        std::move(fileReader),
-        LJParams,
-        domainOrigin,
-        domainSize,
-        cutoff,
-        membraneOrigin,
-        numParticlesWidth,
-        2,
-        k,
-        r_0,
-        spacing,
-        BoundaryConfig(
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW),
-        gravityConst,
-        0,
-        0,
-        0);
+    // Generate expected forces
+    std::vector<std::array<double, 3>> expectedForces(container.particles.size(), { 0, 0, 0 });
+    for (int i = 0; i < container.particles.size(); i++)
+        for (int j = 0; j < container.particles.size(); j++)
+            if (i != j) {
+                // The r0 to use depends on if the particles are aligned diagonally
+                double actualR0 =
+                    (container.particles[i].getX()[0] == container.particles[j].getX()[0] ||
+                     container.particles[i].getX()[1] == container.particles[j].getX()[1])
+                        ? r0
+                        : std::sqrt(2) * r0;
+                expectedForces[i] =
+                    expectedForces[i] +
+                    harmonicPotential(container.particles[i], container.particles[j], actualR0, k);
+            }
 
-    force_membrane(sim);
-
-    unsigned pCount = 0;
-    for (auto p : particles) {
-        for (unsigned i = 0; i < 3; i++) {
-            EXPECT_NEAR(p.getF().at(i), expectedFs.at(pCount).at(i), PRESICION)
-                << "Particle " << pCount << " Dimension " << i << ": Expected "
-                << expectedFs.at(pCount).at(i) << " but got: " << p.getF().at(i);
-        }
-        ++pCount;
-    }
+    // Check if the forces match
+    for (int i = 0; i < container.particles.size(); i++)
+        for (int j = 0; j < 3; j++)
+            EXPECT_NEAR(container.particles[i].getF().at(j), expectedForces[i].at(j), PRESICION);
 }
 
-// Test forces between molecules particle and non-molecules particles
+// Test intra molecular forces including LJ forces for membrane particles
+// Exact same as above, only see if the forces are different -> thus we know the forces were changed
+// and the LJ Test makes sure the calculation is correct
 TEST_F(calcForceMembrane, calcForceMembraneLJ)
 {
-    // Particles with normed distance and sigma should be calculated correctly
-    double c = std::sqrt(3) / 4;
-    // Form equilateral triangle
-    Particle p1 { { 0, 0, c }, { 0, 0, 0 }, 1, 3 };
-    Particle p2 { { 0, 0.5, -c }, { 0, 0, 0 }, 2, 3 };
-    Particle p3 { { 0, -0.5, -c }, { 0, 0, 0 }, 3, 3 };
+    double r0 = 2;
+    double r = 1;
+    double k = 20;
 
-    particles = std::vector<Particle> { p1, p2, p3 };
+    particles = std::vector<Particle> {};
+    ParticleContainer container { particles };
+    Particle tmp;
+    Membrane mem { z, 2, 2, 1, r, 1, z, 0, 3, 1, r0, k, tmp };
+    mem.initLJParams(1, 2); // big sigma to include repulsive forces
+    mem.generateMolecule(container, 562);
 
-    double epsilon = 3.14159;
-    double sigma = 1;
-    double gravityConst = 9;
+    // Check if the number of particles are correct
+    EXPECT_EQ(container.particles.size(), 4);
 
-    std::array<double, 3> p1F { 0, 0 + 1 * gravityConst, epsilon * 24 * 4 * c };
-    std::array<double, 3> p2F { 0, epsilon * 24 * 1.5 + 2 * gravityConst, epsilon * 24 * -2 * c };
-    std::array<double, 3> p3F { 0, epsilon * 24 * -1.5 + 3 * gravityConst, epsilon * 24 * -2 * c };
-    std::array<std::array<double, 3>, 3> expectedFs = { p1F, p2F, p3F };
+    CellGrid grid { { -5, -5, -5 }, { 10, 10, 10 }, 5 };
+    grid.addParticlesFromContainer(container);
 
-    std::map<unsigned, std::pair<double, double>> LJParams {
-                { 1, { epsilon, sigma }}, { 2, { epsilon, sigma }}, { 3, { epsilon, sigma } }
+    mem.calculateIntraMolecularForces(grid);
+
+    // lambda function to do harmonic potential
+    auto harmonicPotential = [](Particle& p1, Particle& p2, double r0, double k) {
+        return k / ArrayUtils::L2Norm(p2.getX() - p1.getX()) *
+               (ArrayUtils::L2Norm(p2.getX() - p1.getX()) - r0) * (p2.getX() - p1.getX());
     };
 
-    MembraneSimulation sim(
-        start_time,
-        delta_t,
-        end_time,
-        particles,
-        strat,
-        std::move(writer),
-        std::move(fileReader),
-        LJParams,
-        domainOrigin,
-        domainSize,
-        cutoff,
-        { 0, 0, c },
-        1,
-        numParticlesHeight,
-        k,
-        r_0,
-        1,
-        BoundaryConfig(
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW,
-            BoundaryType::OUTFLOW),
-        gravityConst,
-        0,
-        0,
-        0);
+    // Generate expected forces
+    std::vector<std::array<double, 3>> expectedForces(container.particles.size(), { 0, 0, 0 });
+    for (int i = 0; i < container.particles.size(); i++)
+        for (int j = 0; j < container.particles.size(); j++)
+            if (i != j) {
+                // The r0 to use depends on if the particles are aligned diagonally
+                double actualR0 =
+                    (container.particles[i].getX()[0] == container.particles[j].getX()[0] ||
+                     container.particles[i].getX()[1] == container.particles[j].getX()[1])
+                        ? r0
+                        : std::sqrt(2) * r0;
+                expectedForces[i] =
+                    expectedForces[i] +
+                    harmonicPotential(container.particles[i], container.particles[j], actualR0, k);
+            }
 
-    force_membrane(sim);
-
-    EXPECT_EQ(sim.container.particles[0].getMembraneId() != -1, true);
-    unsigned pCount = 0;
-    for (auto p : particles) {
-        for (unsigned i = 0; i < 3; i++) {
-            EXPECT_NEAR(p.getF().at(i), expectedFs.at(pCount).at(i), PRESICION)
-                << "Particle " << pCount << " Dimension " << i << ": Expected "
-                << expectedFs.at(pCount).at(i) << " but got: " << p.getF().at(i);
-        }
-        ++pCount;
-    }
+    // Check if the forces don't match -> as LJ was added
+    for (int i = 0; i < container.particles.size(); i++)
+            EXPECT_TRUE(ArrayUtils::L2Norm(container.particles[i].getF() - expectedForces[i]) > PRESICION);
 }
