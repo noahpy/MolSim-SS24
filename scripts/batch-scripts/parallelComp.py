9c5e0e9cdb10d795998804d11c8c4e44297f2475
noahpy <noschl@proton.me>
2024-07-17 11:29:42 +0200

    update batch script

--- scripts/batch-scripts/parallelComp.py
+++ scripts/batch-scripts/parallelComp.py
@@ -1,6 +1,7 @@
 
 import subprocess
 
+
 def get_run_script(name: str, num_threads: str, input_file: str,
                    clusters: str = "cm2_tiny", partition: str = "cm2_tiny") -> str:
     SCRIPT = f"""#!/bin/bash
@@ -23,8 +24,13 @@ OMP_NUM_THREADS={num_threads} src/MolSim {input_file} -x -s 4
 if __name__ == "__main__":
     for num_threads in [1, 2, 4, 8, 16, 28, 56]:
         name = f"parallel_{num_threads}"
-        run_script = get_run_script(
-            name, num_threads, "../input/reyleigh_3D_short.xml")
+        run_script = ""
+        if (num_threads < 16):
+            run_script = get_run_script(
+                name, num_threads, "../input/reyleigh_3D_short.xml")
+        else:
+            run_script = get_run_script(
+                name, num_threads, "../input/reyleigh_3D_short.xml", "cm2", "cm2_std")
         with open(f"{name}.sh", "w") as f:
             f.write(run_script)
 
